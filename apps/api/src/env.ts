249b194210b2cbbc479327b6e43a5d39a66980c0
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2024-05-24 17:10:24 -0300

    feat: banco de dados com postgresql e prisma

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import z from 'zod'
 
-dotenv.config({ path: '.env.local' })
+dotenv.config({ path: '.env' })
 
 const envSchema = z.object({
   SERVER_PORT: z.string(),
